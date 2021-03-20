36b07d54a56a48976f7d96a43a956f42df132504
Nikhil Naik <nnaik@mail.usf.edu>
2021-03-20 19:26:38 -0400

    Updates

---
+++ precache-manifest.5fd7efe4f6ae8005b9c795aece85fb88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b197ec8da6cf933780d6832780136d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfcb0d3a6e850dbc379c",
+    "url": "/static/css/main.ea21fe09.chunk.css"
+  },
+  {
+    "revision": "5842c9f7435d7db91806",
+    "url": "/static/js/2.75fc4720.chunk.js"
+  },
+  {
+    "revision": "e6179b79dd42f3d6eb40d5fd9e64188c",
+    "url": "/static/js/2.75fc4720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfcb0d3a6e850dbc379c",
+    "url": "/static/js/main.1b4d5d7d.chunk.js"
+  },
+  {
+    "revision": "d420ad8802edeb91ed61",
+    "url": "/static/js/runtime-main.7f8a04c9.js"
+  },
+  {
+    "revision": "04073341f5916e00619af629dae5672d",
+    "url": "/static/media/trophie.04073341.svg"
+  }
+]);
