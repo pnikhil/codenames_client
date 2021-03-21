ca142e48da9236c5494a7f82a7946d76ddba44e8
Nikhil Naik <nnaik@mail.usf.edu>
2021-03-21 16:07:48 -0400

    changed router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, {useEffect} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-
 import Join from './components/Join'
 import Game from './components/Game'
 import './css/App.css';
 import './css/Button.css';
 
 const App = () => {
+    return (
+        useEffect(() => {
+            document.title = 'Codenames'
+        }, []),
 
-  return (
-
-      useEffect(() => {
-          document.title = 'Codenames'
-      }, []),
-
-      <Router basename={'/'}>
-        <Route path='/' exact component={Join} />
-        <Route path='/play/:channel' component={Game} />
-      </Router>
-  );
-
+            <Router basename={'/'}>
+                <Route path='/' exact component={Join} />
+                <Route path='/play/:channel' component={Game} />
+            </Router>
+    );
 }
-
 export default App;
