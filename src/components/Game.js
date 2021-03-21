9323156fe6301e90ae1042203b6d472412ea8775
Nikhil Naik <nnaik@mail.usf.edu>
2021-03-21 19:15:22 -0400

    Changed css

--- src/components/Game.js
+++ src/components/Game.js
@@ -188,7 +188,7 @@ const Game = ({location}) => {
 						</span>
                                     }
 
-                                <span className="currentTurn timer">Timer</span>
+                                {/*<span className="currentTurn timer">Timer</span>*/}
                                 {puzzle.points && <div className="scores scoreBackground">
 
                                     <svg xmlns="http://www.w3.org/2000/svg">
