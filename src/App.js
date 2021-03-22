86903ae5206321f63655321e8232fd5324301157
Nikhil Naik <nnaik@mail.usf.edu>
2021-03-22 19:38:05 -0400

    added 404 error page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import {Route, BrowserRouter as Router} from 'react-router-dom'
+import {Route, Switch, BrowserRouter as Router} from 'react-router-dom'
 
 import Join from './components/Join'
 import Game from './components/Game'
 import Footer from './components/Footer'
 import './css/Button.css';
+import NotFoundPage from "./components/NotFoundPage";
 
 const App = () => {
 
   return (
       <Router basename={'/'}>
+          <Switch>
         <Route path='/' exact component={Join} />
-        <Route path='/play/:channel' component={Game} />
+        <Route path='/play/:channel' exact component={Game} />
+          <Route path="*" component={NotFoundPage} />
+              </Switch>
         <Footer />
       </Router>
   );
